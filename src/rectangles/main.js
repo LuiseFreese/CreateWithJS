a671444a6019b40bc2b386ea688da2568429e233
Luise Freese <luise@raeuberleiterin.de>
2021-09-20 22:21:14 +0200

    STYLE little clean up

--- src/rectangles/main.js
+++ src/rectangles/main.js
@@ -1,18 +1,6 @@
 let canvas = document.querySelector('canvas');
 let context = canvas.getContext('2d');
 
-context.fillStyle = '#FFE47A';
-// context.fillRect(100, 100, 400, 400);
-
-context.lineWidth = 4;
-context.beginPath();
-context.rect(100, 100, 400, 400);
-// context.stroke();
-
-context.beginPath();
-context.arc(300, 300, 100, 0, Math.PI * 2);
-// context.stroke();
-
 const width = 100;
 const height = 100;
 const gap = 20;
